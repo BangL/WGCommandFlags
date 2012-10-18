fec4652180526f0a6f32cd1689aa2d53903739ea
BangL <henno.rickowski@googlemail.com>
2012-10-18 19:08:01 +0200

    cleanup

--- de/bangl/wgcf/Utils.java
+++ de/bangl/wgcf/Utils.java
@@ -56,7 +56,7 @@ public final class Utils {
             return new HashSet<>();
         }
     }
-    
+
     public static HashSet<String> getMergedFlag(WorldGuardPlugin wgp, CustomSetFlag flag, Player player, Location loc)
     {
         //LocalPlayer wgPlayer = wgp.wrapPlayer(player);
