ccc718f3be8f406c18f65e9b183bcc0c9ce77e18
BangL <henno.rickowski@googlemail.com>
2012-10-18 23:32:48 +0200

    cleanup

--- de/bangl/wgcf/WGCommandFlagsPlugin.java
+++ de/bangl/wgcf/WGCommandFlagsPlugin.java
@@ -46,10 +46,13 @@ public class WGCommandFlagsPlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
+
         // we nullify all vars, cause it could be a server reload and we don't wanna leave trash in our expensive RAM.
         this.pluginWGCustomFlags = null;
         this.pluginWorldGuard = null;
         this.listenerPlayer = null;
+
+        saveConfig();
     }
 
 }
